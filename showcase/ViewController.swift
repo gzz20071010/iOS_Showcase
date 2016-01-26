c19a1a071a4499c638ce732ce3cf097c2c073c87
shengxiangguo <sg3214@nyu.edu>
2016-01-26 12:20:17 -0500

    added fbSDK, updated info.plist

--- showcase/ViewController.swift
+++ showcase/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func fbButtonPressed(sender: UIButton!){
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            if facebookError != nil {
+                print("Facebook login failed. \(facebookError)")
+            }else {
+                let accessTocken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully loged in with Facebook. \(accessTocken)")
+            }
+        }
+    }
 }
 
