9217d7255ecf2a0248324311847d144592dd360f
shengxiangguo <sg3214@nyu.edu>
2016-01-26 09:20:43 -0500

    added fonts, costume class

---
+++ showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  showcase
+//
+//  Created by shengxiang guo on 1/25/16.
+//  Copyright © 2016 shengxiang guo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
---
+++ showcase/materialView.swift
@@ -0,0 +1,19 @@
+//
+//  materialView.swift
+//  showcase
+//
+//  Created by shengxiang guo on 1/25/16.
+//  Copyright © 2016 shengxiang guo. All rights reserved.
+//
+
+import UIKit
+
+class materialView: UIView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+}
